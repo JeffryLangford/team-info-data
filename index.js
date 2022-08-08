5db112972a6efc5be6bf8d261b54d5664d4a76c1
Jeffry Langford <jblangford97@gmail.com>
2022-08-08 17:07:09 -0500

    final changes

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ import inquirer from 'inquirer';
 import * as fs from 'fs';
 
 import generateHTML from './src/generate-html.js';
+import Engineer from './lib/Engineer.js';
+import Manager from './lib/Manager.js';
+import Intern from './lib/Intern.js';
+import Employee from './lib/Employee.js';
+
+const employees = [];
 
 const questions = [
     {
@@ -31,8 +37,9 @@ const questions = [
         message: 'What is their email?'
     },
     {
+        when: (choice) => choice.role === 'Manager',
         type: 'input',
-        name: 'office-number',
+        name: 'officeNumber',
         message: 'If a Manager, what is their Office Number? If not, hit enter',
         validate(value) {
             const valid = !isNaN(parseFloat(value));
@@ -41,11 +48,13 @@ const questions = [
         filter: Number,
     },
     {
+        when: (choice) => choice.role === 'Engineer',
         type: 'input',
         name: 'github',
         message: 'If an Engineer, what is their Github Username? If not, hit enter',
     },
     {
+        when: (choice) => choice.role === 'Intern',
         type: 'input',
         name: 'school',
         message: 'If an Intern, what is their School? (If not, hit enter)',
@@ -60,10 +69,19 @@ const questions = [
 
 const ask = () => {
     inquirer.prompt(questions).then((data) => {
+        if(data.role == 'Intern') {
+            employees.push(new Intern(data))
+        } else if (data.role == 'Manager') {
+            employees.push(new Manager(data))
+        } else if (data.role == 'Engineer') {
+            employees.push(new Engineer(data))
+        } else if (data.role == 'Employee') {
+            employees.push(new Employee(data.name, data.id, data.email))
+        }
         if (data.askAgain) {
             ask()
         } else {
-        fs.writeFile('team-info.html', generateHTML(data), err => {
+        fs.writeFile('team-info.html', generateHTML(employees), err => {
             if (err) throw err;
         })}
     });
--- src/generate-html.js
+++ src/generate-html.js
@@ -1,46 +1,52 @@
-const generateHTML = data => {
-    return `
+import Engineer from '../lib/Engineer.js';
+import Manager from '../lib/Manager.js';
+import Intern from '../lib/Intern.js';
+
+function generateHTML(employees) {
+  let cards = '';
+
+ const generateCard = item => {
+     cards +=`
+    <card class="border border-dark p-2 bg-info text-white">
+      <h2 class="mb-2">${item.name}</h2>
+      <h4>Id: ${item.id}</h4>
+      <h4>Email: ${item.email}</h4>
+    `
+    if(item instanceof Engineer) {
+      cards += `<h4>Github: github.com/${item.github}</h4>`
+    }
+    if(item instanceof Manager) {
+      cards += `<h4>Office Number: ${item.officeNumber}</h4>`
+    }
+    if(item instanceof Intern) {
+      cards += `<h4>School: ${item.school}</h4>`
+    }
+    cards += `</card>`;
+  }
+
+  employees.forEach(element => 
+  generateCard(element))
+
+  return `
     <!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Team Info</title>
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-</head>
-<body class="wrapper bg-light">
-  <header class="text-center p-4 m-4 bg-info">
-    <h1>My Team</h1>
-  </header>
-  <main class="flex-box p-4 m-4">
-    <card class="border border-dark w-25">
-      <h2>${data.name}</h2>
-      <h4>Id: 12345678</h4>
-      <h4>Email: asdfghjk</h4>
-      <h4>Office Number: 12345678</h4>
-    </card>
-    <card class="border border-dark w-25">
-      <h2>Employee's Name</h2>
-      <h4>Id: 12345678</h4>
-      <h4>Email: asdfghjk</h4>
-    </card>
-    <card class="border border-dark w-25">
-      <h2>Engineer's Name</h2>
-      <h4>Id: 12345678</h4>
-      <h4>Email: asdfghjk</h4>
-      <h4>Github: link</h4>
-    </card>
-    <card class="border border-dark w-25">
-      <h2>Intern's Name</h2>
-      <h4>Id: 12345678</h4>
-      <h4>Email: asdfghjk</h4>
-      <h4>School: School</h4>
-    </card>
-  </main>
-  
-</body>
-</html>`
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Team Info</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  </head>
+  <body class="wrapper bg-light">
+    <header class="text-center p-4 m-4 bg-info">
+      <h1>My Team</h1>
+    </header>
+    <main class="d-flex justify-content-around p-4 m-4">
+      ${cards}
+    </main>
+    
+  </body>
+  </html>`
 }
 
 export default generateHTML;
